0a81c0367e8ff9e3adacb18fe83a44b379be15da
vesta-12 <sofiya.serbina@narxoz.kz>
2026-04-28 10:11:54 +0500

    create CommentsHandler

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import org.example.handlers.RegisterHandler;
+import org.example.handlers.LoginHandler;
+import org.example.handlers.LogoutHandler;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,6 +12,8 @@ public class Main {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/register", new RegisterHandler());
+        server.createContext("/login", new LoginHandler());
+        server.createContext("/logout", new LogoutHandler());
         server.createContext("/", exchange -> {
             String response = """
                     <html>
@@ -18,6 +22,7 @@ public class Main {
                             <li><a href="/register">Register</a></li>
                             <li><a href="/login">Login</a></li>
                             <li><a href="/comments">Comments</a></li>
+                            <li><a href="/logout">Logout</a></li>
                         </ul>
                     </body>
                     </html>
